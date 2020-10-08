c120944b787bdf2193567c1c6c954e2c359f110b
Eughami <imamosi50@gmail.com>
2020-10-09 01:15:16 +0300

    Auth Guard

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,9 +4,9 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'taskmanagement',
+  username: 'mbl',
+  password: 'mbl2020',
+  database: 'postgres',
   entities: [__dirname + '/../**/*.entity.ts'],
   synchronize: true,
 };
